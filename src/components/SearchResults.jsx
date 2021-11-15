3ea606813637222e760a2948f9402b1cac92f914
prothy <lukacsurgay@gmail.com>
2021-11-15 22:21:29 +0100

    feat: add additional columns to data grid

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -2,23 +2,25 @@ import { DataGrid } from '@mui/x-data-grid'
 import React, { useEffect, useState } from 'react'
 import _ from 'lodash'
 
-const SearchResults = ({ detailedResultsState, selectedState, modalOpenState }) => {
+const SearchResults = ({ detailedResultsState, selectedState, modalOpenState, resultsLoadingState }) => {
     const [ results, ] = detailedResultsState
     const [, setSelected ] = selectedState
     const [, setModalOpen] = modalOpenState
+    const [loading, ] = resultsLoadingState
 
     const [rows, setRows] = useState([])
 
     const columns = [
         {field: 'id', hide: true},
         {field: 'name', width: 300},
-        {field: 'year', width: 150}
+        {field: 'year', width: 150, align: 'right', headerAlign: 'right'},
+        {field: 'score', width: 150, align: 'right', headerAlign: 'right'}
     ]
 
     useEffect(() => {
         setRows(results.map(movie => {
             movie.year = new Date(movie.releaseDate).getFullYear()
-            return _.pick(movie, ['id', 'name', 'year'])
+            return _.pick(movie, ['id', 'name', 'year', 'score'])
         }))
     }, [results])
 
@@ -31,6 +33,8 @@ const SearchResults = ({ detailedResultsState, selectedState, modalOpenState }) 
                     setSelected(ev.id)
                     setModalOpen(true)
                 }} 
+                loading={loading}
+                hideFooter={true}
             />
         </div>
     )
